4b9cd51a403d19af351d73f41d5f1428b777c5da
Taimoor Hassan <taimoor.hassan10@yahoo.com>
2023-05-18 20:17:52 +0500

    updated build scrpit

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
+echo "RAILS_MASTER_KEY: $RAILS_MASTER_KEY"
+echo "DATABASE_URL: $DATABASE_URL"
+
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
