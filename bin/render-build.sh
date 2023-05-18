3592362c0af4594fff90b33ae1591b43d142e4b5
Taimoor Hassan <taimoor.hassan10@yahoo.com>
2023-05-18 20:39:54 +0500

    updated render build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,7 @@ set -o errexit
 echo "RAILS_MASTER_KEY: $RAILS_MASTER_KEY"
 echo "DATABASE_URL: $DATABASE_URL"
 
-bundle install --trace
+bundle install
 bundle exec rake assets:precompile --trace
 bundle exec rake assets:clean --trace
 bundle exec rake db:migrate --trace
